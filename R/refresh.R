579a937411f06a23efebeb67f2e918a477893623
Caillard mathias <107404704+OverealDev@users.noreply.github.com>
2022-06-29 07:35:22 +0200

    Update refresh.R

--- R/refresh.R
+++ R/refresh.R
@@ -10,8 +10,8 @@
 #' @export
 refresh <- function() {
 
-  if ("dev/app.R" %in% list.files(here::here(), recursive = T)) {
-    Sys.setFileTime("dev/app.R", Sys.time())
+  if ("app.R" %in% list.files(here::here(), recursive = T)) {
+    Sys.setFileTime("app.R", Sys.time())
   } else {
     stop("Couldn't locate 'app.R' in /dev (if you are working in a golem, rename run_dev.R)")
   }
